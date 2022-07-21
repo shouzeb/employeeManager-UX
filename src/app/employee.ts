ec7038e81efbcdf600737b6a6cfeba518c932a28
Syed Shouzeb Hassan <38203938+shouzeb@users.noreply.github.com>
2022-07-21 16:22:55 +0500

    new files

---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee{
+
+    id: number;
+    name: string;
+    email: string;
+    jobTitle: String;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+
+}
